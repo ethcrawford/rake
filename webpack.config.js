1ff6eeba727174abb7cf0cb48874cfd77681df4f
Ethan Crawford <ethcrawford@gmail.com>
2018-09-12 13:30:19 +0300

    Add Babel

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,20 @@ const common = merge([
     output: {
       filename: "main.js",
       path: path.resolve(__dirname, "dist")
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        }
+      ]
     }
   },
   addPage("index"),
