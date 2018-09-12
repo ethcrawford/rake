bef2e21b141df0d96751f78c7f479fca00287db3
Ethan Crawford <ethcrawford@gmail.com>
2018-09-12 12:32:01 +0300

    Add html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,39 @@
-const path = require('path');
-const merge = require('webpack-merge');
+const path = require("path");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+function addPage(page) {
+  const filename = page + ".html";
+  const template = path.resolve(__dirname, "src", filename);
+
+  return {
+    plugins: [
+      new HtmlWebpackPlugin({
+        entry: "main",
+        filename: filename,
+        template: template
+      })
+    ]
+  };
+}
 
 const common = merge([
   {
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist')
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist")
     }
-  }
+  },
+  addPage("index"),
+  addPage("one"),
+  addPage("two")
 ]);
 
 module.exports = function(env) {
-  if (env === 'development') {
-    return merge([common, { mode: 'development' }]);
+  if (env === "development") {
+    return merge([common, { mode: "development" }]);
   } else {
-    return merge([common, { mode: 'production' }]);
+    return merge([common, { mode: "production" }]);
   }
 };
